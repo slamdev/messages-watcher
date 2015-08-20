3f3ab3db360e93341b572554111c67bc9c956139
slamdev <slam.ul73@gmail.com>
2015-08-20 17:47:41 +0300

    add weld for CDI

--- collector/runner/src/main/java/com/github/slamdev/messageswatcher/collector/runner/Runner.java
+++ collector/runner/src/main/java/com/github/slamdev/messageswatcher/collector/runner/Runner.java
@@ -1,10 +1,31 @@
 package com.github.slamdev.messageswatcher.collector.runner;
 
 import com.github.slamdev.messageswatcher.collector.capturer.spi.Collector;
+import org.jboss.weld.environment.se.StartMain;
+import org.jboss.weld.environment.se.events.ContainerInitialized;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.enterprise.event.Observes;
+import javax.enterprise.inject.Any;
+import javax.enterprise.inject.Instance;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+@Singleton
 public class Runner {
+
+    @Inject
+    @Any
+    private Instance<Collector> collectors;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Runner.class);
+
     public static void main(String[] args) {
-        Collector collector = null;
-        System.out.print("runner" + collector);
+        StartMain.main(args);
+    }
+
+    public void run(@Observes ContainerInitialized event) {
+        collectors.forEach(e -> LOGGER.info(e.toString()));
     }
 }
